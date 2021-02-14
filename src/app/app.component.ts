8bc522393029e9e6d5fb402ca2a357c477c93674
Pooja Bansal <pooja.anoop.bansal@gmail.com>
2021-02-14 15:36:08 -0500

    POS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,49 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'my-app';
-  checkboxUIElements: string[];
-  removeCheckBox: string[] = ["Jacket", "Belt"];
-  unPackItem: string;
-  item: string;
-  packItem: string;
-  isFocus: boolean;
-  isError: boolean;
-
+ 
+tabName =['DESCRIPTION','INSIGHT','APPLICANTS','NOTIFICATIONS'];
+selectedTab=0;
   constructor() {}
 
   ngOnInit() {
-    this.checkboxUIElements = [
-      'Pants',
-      'Macbook',
-      'Hat',
-      'T-shirt'
-    ];
+    
   }
 
-  itemChange(event: any){
-    this.isError = event?false:true;
-
-  }
-
-  addItem() {
-    if(this.item) {
-      this.checkboxUIElements.push(this.item);
-    } else {
-      this.isError = true;
-    }
-  }
-  onFocus(){
-    this.isFocus=true;
-  }
-
-  removeFruit(index: number, itemNo: number) {
-    if (itemNo === 1) {
-      this.removeCheckBox.push(this.checkboxUIElements[index]);
-      this.checkboxUIElements.splice(index, 1);
-    } else {
-      this.checkboxUIElements.push(this.removeCheckBox[index]);
-      this.removeCheckBox.splice(index, 1);
-    }
+  itemClicked(i:number) {
+    this.selectedTab = i;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { FilterItemsPipe } from './filter-items.pipe';
 
 @NgModule({
-  declarations: [AppComponent, FilterItemsPipe],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
